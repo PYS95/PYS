a3267221adfb494390281da34152247e91f6febc
YoungSun <pys9764@naver.com>
2023-05-31 14:59:07 +0900

    hi

---
+++ while2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i=0;
+    int j=0;
+    while(i<5)
+    {
+        while(j<i)
+        {
+            printf("0");
+            j++;
+        }
+        printf("*\n");
+        i++;
+        j=0;
+
+    }
+    
+    return 0;
+}
